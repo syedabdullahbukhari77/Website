4954cf8a1417ac47534241fb3b4f195fb58ee701
Syed Abdullah Bukhari <syedabdullahbukhari77@gmail.com>
2024-07-18 00:59:59 +0500

    Message

---
+++ server/main.py
@@ -0,0 +1,26 @@
+from flask import Flask  , jsonify, render_template 
+from flask_cors import CORS
+
+
+app =  Flask (__name__)
+cors = CORS(app, origins='*')
+
+@app.route('/Home')
+def Home () :
+    return render_template('index.html')
+
+@app.route ('/api/users' , methods=['GET'])
+def users():
+    return jsonify(
+        {
+            'users' : [
+                'Syed Abdullah Bukari',
+                'Syed Hussain',
+                'Umar',
+                'Abu bakr'
+            ]
+        }
+    )
+
+if  __name__ == "__main__":
+    app.run (debug=True , port=3000)
